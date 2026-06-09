a54bdcf38821db0d7961c6f082d174da93b4654b
mahii0509 <mahii0509@gmail.com>
2026-06-09 13:58:56 +0530

    Added Project

---
+++ Smart-CI-Failure-Analyzer-Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Smart_CI_Failure_Analyzer_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
