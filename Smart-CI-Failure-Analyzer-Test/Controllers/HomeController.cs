9a12f0a7aa58dd1bdea66e75867ea18a7cc084b2
mahii0509 <mahii0509@gmail.com>
2026-06-10 11:24:36 +0530

    Added Project

--- Smart-CI-Failure-Analyzer-Test/Controllers/HomeController.cs
+++ Smart-CI-Failure-Analyzer-Test/Controllers/HomeController.cs
@@ -17,12 +17,12 @@ namespace Smart_CI_Failure_Analyzer_Test.Controllers
         {
             return View();
         }
-        }
+        
         public IActionResult Privacy()
         {
             return View();
         }
-
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
