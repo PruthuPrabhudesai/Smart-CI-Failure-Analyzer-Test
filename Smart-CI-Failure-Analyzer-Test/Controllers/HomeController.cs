da350a811778ac741646efaa105865a37086a0c5
Pruthu <pruthu373@gmail.com>
2026-06-25 12:22:38 +0530

    testing for AI integration new

--- Smart-CI-Failure-Analyzer-Test/Controllers/HomeController.cs
+++ Smart-CI-Failure-Analyzer-Test/Controllers/HomeController.cs
@@ -19,22 +19,22 @@ namespace Smart_CI_Failure_Analyzer_Test.Controllers
             return View(); 
         }
 
-        public IActionResult Privacy()
-        {
-            NotExistingMethod();
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    NotExistingMethod();
+        //    return View();
+        //}
         //public IActionResult Privacy()
         //{
         //    return View();
         //}
 
-        //public IActionResult Privacy()
-        //{
-        //    var model = new ErrorViewModel();
-        //    model.InvalidProperty = "Test";
-        //    return View();
-        //}
+        public IActionResult Privacy()
+        {
+            var model = new ErrorViewModel();
+            model.InvalidProperty = "Test";
+            return View();
+        }
 
         //public IActionResult Privacy()
         //{
