453c8a24453eb9ed27edce24da41c346b479d814
Pruthu <pruthu373@gmail.com>
2026-06-25 12:15:39 +0530

    testing flow

--- Smart-CI-Failure-Analyzer-Test/Controllers/HomeController.cs
+++ Smart-CI-Failure-Analyzer-Test/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Smart_CI_Failure_Analyzer_Test.Controllers
         public IActionResult Index()
         {
 
-            return View(add);
+            return View(add); 
         }
 
         //public IActionResult Privacy()
